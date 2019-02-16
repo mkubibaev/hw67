afa25bc82cf052792fd87b8f29abe40839d35d28
mkubibaev <mkubibaev@gmail.com>
2019-02-17 00:51:20 +0600

    deleted button component

--- src/components/CalcDisplay/CalcDisplay.js
+++ src/components/CalcDisplay/CalcDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CalcDisplay = props => (
     <div className="calc-display">
-        <div className="result">{props.result ? `= ${props.result}` : ''}</div>
+        <div className="result">{props.result}</div>
         <div className="operand">{props.operator} {props.secondOperand}</div>
         <div className="operand">{props.firstOperand ? props.firstOperand : 0}</div>
     </div>
